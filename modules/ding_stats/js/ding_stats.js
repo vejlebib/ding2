292153d7dca8b004d81f41fb134280958733e7e5
Simon Holt <simonholt83@gmail.com>
2018-11-09 00:46:42 +0100

    Implement cta events

--- modules/ding_stats/js/ding_stats.js
+++ modules/ding_stats/js/ding_stats.js
@@ -4,18 +4,21 @@
 
 (function($) {
 
+  var postEventData = function(eventData) {
+    $.post(Drupal.settings.basePath + 'ding_stats.php', { data: eventData }, function(result) {
+      // Update cookie if the server generated a new session id for us.
+      if (result.dingStatsId) {
+        var expire =  new Date(new Date().getTime() + 15 * 60 * 1000);
+        Cookies.set('dingStatsId', result.dingStatsId, {
+          expires: expire
+        });
+      }
+    }, 'json');
+  };
+
   // Post event data to the backend.
   var eventData = Drupal.settings.dingStats.eventData;
-  console.log(eventData);
-  $.post(Drupal.settings.basePath + 'ding_stats.php', { data: eventData }, function(result) {
-    // Update cookie if the server generated a new session id for us.
-    if (result.dingStatsId) {
-      var expire =  new Date(new Date().getTime() + 15 * 60 * 1000);
-      Cookies.set('dingStatsId', result.dingStatsId, {
-        expires: expire
-      });
-    }
-  }, 'json');
+  postEventData(eventData);
 
   $(function() {
     // Add get parameters to search result links, so that we can record
@@ -38,6 +41,37 @@
           var seperator = (link.indexOf('?') != -1) ? '&' : '?';
           $(this).attr('href', link + seperator + suffix + rank);
         });
+      });
+    }
+
+    // Click-through events.
+    if (eventData.event_type == 'ct') {
+      // For clicked through events flagged as collections, we need to apply
+      // query pararmeters to object links in the collection to tranfer the
+      // associated data, if the user clicks through the collection.
+      if (eventData.material_type == 'collection') {
+        var suffix = 'search_key=' + encodeURIComponent(eventData.search_key);
+        suffix += '&rank=' + eventData.rank;
+        $('.pane-ting-collection a[href^="/ting/object/"]').each(function() {
+          var link = $(this).attr('href');
+          var seperator = (link.indexOf('?') != -1) ? '&' : '?';
+          $(this).attr('href', link + seperator + suffix);
+        });
+      }
+
+      // For these following AJAX-interaction the events are created client-side
+      // and posted when the interaction takes place.
+      var ctActionEventData = {};
+      ctActionEventData.event_type = 'cta'; // Click-Through-Action event.
+      ctActionEventData.pid = eventData.pid;
+      ctActionEventData.search_key = eventData.search_key;
+      $('.ting-object .action-button.reserve-button').on('touchstart click', function(e) {
+        ctActionEventData.action_type = 'reservation';
+        postEventData(ctActionEventData);
+      });
+      $('.ting-object .ding-list-add-button').on('touchstart click', function(e) {
+        ctActionEventData.action_type = 'bookmark';
+        postEventData(ctActionEventData);
       });
     }
   });
