fc0087c40c3547f47c594e40a0beb9e9464ef841
holt83 <simoh@vejlebib.dk>
2019-04-24 11:46:11 +0200

    Ensure search field is shown on vejlebib frontpage

---
+++ themes/vejlebib/template.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Template file for Vejlebib sub-theme.
+ */
+
+/**
+ * Implements hook_preprocess_html().
+ */
+function vejlebib_preprocess_html(&$vars) {
+  // Ensure the search field is shown on vejlebib frontpage. Note that this
+  // wouldn't be an issue if ddbasic used drupal_is_front_page() instead of
+  // checking for specific path. At this point in the code's life cycle it makes
+  // sense to just handle it here in our own sub-theme.
+  if (drupal_is_front_page()) {
+    $key = array_search('extended-search-is-not-open', $vars['classes_array']);
+    if ($key !== FALSE) {
+      unset($vars['classes_array'][$key]);
+    }
+  }
+}
